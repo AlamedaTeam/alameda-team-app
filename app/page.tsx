091ba4ec0a2fb41d19d77c2159ab21e8087020d1
AlamedaTeam <amartincasero@gmail.com>
2025-08-26 20:18:17 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{display:'grid',gap:16,placeItems:'center',padding:'40px'}}>
-      <img src="/icons/icon-192.png" alt="Alameda Team" width={96} height={96}/>
+      <img src="/icon-192.png" alt="Alameda Team" width={96} height={96}/>
       <h1>Alameda Team</h1>
       <a href="/login">Entrar</a>
       <a href="/profile">Mi perfil</a>
