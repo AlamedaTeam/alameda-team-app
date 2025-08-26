3b9ce3defe3911bde7954b906814f5abdeef32aa
AlamedaTeam <amartincasero@gmail.com>
2025-08-26 22:47:06 +0200

    Create page.tsx

---
+++ app/hello/page.tsx
@@ -0,0 +1,9 @@
+export default function Hello() {
+  return (
+    <main className="hero">
+      <div className="card">
+        <h1>Hola desde Alameda Team 👋</h1>
+      </div>
+    </main>
+  )
+}
