a74ebc8803fce7b5b17f854dc37ca6ffd3b57d1b
AlamedaTeam <amartincasero@gmail.com>
2025-08-26 23:13:34 +0200

    Delete app/hello directory

--- app/hello/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Hello() {
-  return (
-    <main className="hero">
-      <div className="card">
-        <h1>Hola desde Alameda Team 👋</h1>
-      </div>
-    </main>
-  );
-}
